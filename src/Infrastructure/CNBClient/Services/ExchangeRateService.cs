f3ab2cf14f343438a3905d5ccd224caed7de4b1d
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2022-03-24 15:32:57 +0100

    Apply solution wide code standards
    While the project is still not too grown in size, it seemed appropriate to apply the editorconfig rules solution wide

--- src/Infrastructure/CNBClient/Services/ExchangeRateService.cs
+++ src/Infrastructure/CNBClient/Services/ExchangeRateService.cs
@@ -5,13 +5,10 @@ namespace ExchangeRate.Infrastructure.CNBClient.Services;
 public class ExchangeRateService
 {
     /// <summary>
-    /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
-    /// by the source, do not return calculated exchange rates. E.g. if the source contains "CZK/USD" but not "USD/CZK",
-    /// do not return exchange rate "USD/CZK" with value calculated as 1 / "CZK/USD". If the source does not provide
-    /// some of the currencies, ignore them.
+    ///     Should return exchange rates among the specified currencies that are defined by the source. But only those defined
+    ///     by the source, do not return calculated exchange rates. E.g. if the source contains "CZK/USD" but not "USD/CZK",
+    ///     do not return exchange rate "USD/CZK" with value calculated as 1 / "CZK/USD". If the source does not provide
+    ///     some of the currencies, ignore them.
     /// </summary>
-    public IEnumerable<Domain.Entities.ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
-    {
-        return Enumerable.Empty<Domain.Entities.ExchangeRate>();
-    }
+    public IEnumerable<Domain.Entities.ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies) => Enumerable.Empty<Domain.Entities.ExchangeRate>();
 }
